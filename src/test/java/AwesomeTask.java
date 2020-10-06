8955a8e66cba3b564339a77595f259117b79aea3
ZaferBek <zaferbek22@gmail.com>
2020-10-06 20:47:00 +0100

    Added some lines of code before commit

---
+++ src/test/java/AwesomeTask.java
@@ -0,0 +1,7 @@
+public class AwesomeTask {
+
+    // Red means it's untracked
+    //Green means it is under staging area
+    //Grey in .idea folder means it is ignored
+    //When the color is normal
+}
