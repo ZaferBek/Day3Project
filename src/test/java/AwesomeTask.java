d2afec5db79f71dd4b02c8cfc55b54dcb4d81f81
ZaferBek <zaferbek22@gmail.com>
2020-10-06 20:59:39 +0100

    Added more awesome changes

--- src/test/java/AwesomeTask.java
+++ src/test/java/AwesomeTask.java
@@ -4,4 +4,9 @@ public class AwesomeTask {
     //Green means it is under staging area
     //Grey in .idea folder means it is ignored
     //When the color is normal
+
+    //I just pulled new changes from github that I did not have locally
+    //and now my project is in sync with remote perository
+
+    //Now I made more changes
 }
